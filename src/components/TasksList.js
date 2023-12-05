f47e249009f3c9c05ca621f0c83b5f8f94d8da0e
Cristi Daniel <danielcristi735@yahoo.com>
2023-12-05 12:57:38 +0200

    allow sorting tasks

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,17 +1,55 @@
+import { useState } from "react";
 import Task from "./Task";
 
+// Set order for sorting
+const statusOrder = ["Completed", "Overdue", "Upcoming", "Cancelled"];
+
 function TasksList({ tasks, onDeleteTasks, onUpdateTasks }) {
+  const [sortBy, setSortBy] = useState("input");
+  const [filterBy, setFilterBy] = useState("input");
+
+  let sortedTasks;
+  if (sortBy === "input") sortedTasks = tasks;
+
+  if (sortBy === "title")
+    sortedTasks = tasks.slice().sort((a, b) => a.title.localeCompare(b.title));
+
+  if (sortBy === "note")
+    sortedTasks = tasks.slice().sort((a, b) => a.note.localeCompare(b.note));
+
+  if (sortBy === "status") {
+    sortedTasks = tasks.slice().sort((a, b) => {
+      const statusA = statusOrder.indexOf(a.status);
+      const statusB = statusOrder.indexOf(b.status);
+
+      return statusA - statusB;
+    });
+  }
   return (
-    <ul className="tasks_container">
-      {tasks.map((task) => (
-        <Task
-          task={task}
-          key={task.id}
-          onDeleteTasks={onDeleteTasks}
-          onUpdateTasks={onUpdateTasks}
-        />
-      ))}
-    </ul>
+    <>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="title">Sort by title</option>
+        <option value="note">Sort by notes</option>
+        <option value="status">Sort by status</option>
+      </select>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="title">Sort by title</option>
+        <option value="note">Sort by notes</option>
+        <option value="status">Sort by status</option>
+      </select>
+      <ul className="tasks_container">
+        {sortedTasks.map((task) => (
+          <Task
+            task={task}
+            key={task.id}
+            onDeleteTasks={onDeleteTasks}
+            onUpdateTasks={onUpdateTasks}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
 
