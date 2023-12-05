ab3c355e029010eaadb4070a5bcab631419a4290
Cristi Daniel <danielcristi735@yahoo.com>
2023-12-05 11:31:25 +0200

    fixed possibility to filter

--- src/App.js
+++ src/App.js
@@ -1,49 +1,118 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CustomModal from "./components/CustomModal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import TasksList from "./components/TasksList";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  // Filter tasks state
+  const [selectedPriority, setSelectedPriority] = useState("all");
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  // Count all tasks
   const numTasks = tasks.length;
 
+  const filterPriorityTasks = (tasks, priority) =>
+    tasks.filter((task) => task.priority === priority);
+
+  // Filter tasks by priority
+  const lowTasks = filterPriorityTasks(tasks, "low");
+  const mediumTasks = filterPriorityTasks(tasks, "medium");
+  const highTasks = filterPriorityTasks(tasks, "high");
+
   // Count tasks by priority
-  const countPriorityTasks = (tasks, priority) =>
-    tasks.filter((task) => task.priority === priority).length;
-  const lowTasks = countPriorityTasks(tasks, "low");
-  const mediumTasks = countPriorityTasks(tasks, "medium");
-  const highTasks = countPriorityTasks(tasks, "high");
+  const countLowTasks = lowTasks.length;
+  const countMediumTasks = mediumTasks.length;
+  const countHighTasks = highTasks.length;
+
+  useEffect(() => {
+    const updateFilteredTasks = () => {
+      switch (selectedPriority) {
+        case "low":
+          setFilteredTasks(filterPriorityTasks(tasks, "low"));
+          break;
+        case "medium":
+          setFilteredTasks(filterPriorityTasks(tasks, "medium"));
+          break;
+        case "high":
+          setFilteredTasks(filterPriorityTasks(tasks, "high"));
+          break;
+        default:
+          setFilteredTasks(tasks);
+      }
+    };
+
+    updateFilteredTasks();
+  }, [tasks, selectedPriority]);
 
   // CRUD operations
   function handleSetTasks(task) {
     setTasks((tasks) => [...tasks, task]);
   }
+
   function handleDeleteTasks(id) {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this task?"
+    );
+    if (confirmed) {
+      setTasks((tasks) => tasks.filter((task) => task.id !== id));
+    }
   }
-  // console.log(tasks);
+
   return (
     <div className="App">
       <Header numTasks={numTasks} />
       <div className="main_form_container">
         <div className="header_form_container">
           <h2 className="subtitle">Your tasks:</h2>
-          <span className="priority_label">All: {numTasks}</span>
-          <span className="priority_label">Low Priority: {lowTasks}</span>
-          <span className="priority_label">Medium Priority: {mediumTasks}</span>
-          <span className="priority_label">High Priority: {highTasks}</span>
+          <span
+            className={`priority_label ${
+              selectedPriority === "all" ? "active" : ""
+            }`}
+            onClick={() => setSelectedPriority("all")}
+          >
+            All: {numTasks}
+          </span>
+          <span
+            className={`priority_label ${
+              selectedPriority === "low" ? "active" : ""
+            }`}
+            onClick={() => setSelectedPriority("low")}
+          >
+            Low Priority: {countLowTasks}
+          </span>
+          <span
+            className={`priority_label ${
+              selectedPriority === "medium" ? "active" : ""
+            }`}
+            onClick={() => setSelectedPriority("medium")}
+          >
+            Medium Priority: {countMediumTasks}
+          </span>
+          <span
+            className={`priority_label ${
+              selectedPriority === "high" ? "active" : ""
+            }`}
+            onClick={() => setSelectedPriority("high")}
+          >
+            High Priority: {countHighTasks}
+          </span>
+
           <CustomModal title={"Add a task"}>
             <Form onSetTasks={handleSetTasks} />
           </CustomModal>
         </div>
         <div className="body_form_container">
-          {numTasks === 0 ? (
+          {filteredTasks.length === 0 ? (
             "No tasks"
           ) : (
-            <TasksList tasks={tasks} onDeleteTasks={handleDeleteTasks} />
+            <TasksList
+              tasks={filteredTasks}
+              onDeleteTasks={handleDeleteTasks}
+            />
           )}
         </div>
       </div>
